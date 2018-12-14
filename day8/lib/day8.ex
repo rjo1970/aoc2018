3392d605a6e419df1dd5b44b89d3c1401a7b6ac1
RJ Osborne <rjo1970@gmail.com>
2018-12-14 17:59:03 -0500

    created day8

---
+++ day8/lib/day8.ex
@@ -0,0 +1,18 @@
+defmodule Day8 do
+  @moduledoc """
+  Documentation for Day8.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day8.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
