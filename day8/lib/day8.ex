3b1c76ed247827fbf965ac963ca99668a8dc9e27
RJ Osborne <rjo1970@gmail.com>
2018-12-15 00:22:02 -0500

    first start of day8

--- day8/lib/day8.ex
+++ day8/lib/day8.ex
@@ -1,18 +1,85 @@
 defmodule Day8 do
-  @moduledoc """
-  Documentation for Day8.
-  """
+  def puzzle_data() do
+    File.stream!("puzzle.txt")
+    |> Enum.map(fn x -> String.split(x, " ") end)
+    |> List.flatten()
+    |> Enum.map(fn x -> String.to_integer(x) end)
 
-  @doc """
-  Hello world.
+    # [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+  end
 
-  ## Examples
+  def parse(data) when is_list(data) do
+    IO.inspect(data)
 
-      iex> Day8.hello()
-      :world
+    [count, metacount] =
+      data
+      |> Enum.take(2)
 
-  """
-  def hello do
-    :world
+    next =
+      data
+      |> Enum.drop(2)
+
+    IO.inspect({[count], [metacount], [], next})
+    parse({[count], [metacount], [], next})
+  end
+
+  def parse({[], [], metadata, []}) do
+    IO.inspect({:donedone})
+    metadata
+  end
+
+  def parse({[], [n | metacounts], metadata, data}) do
+    more_meta =
+      data
+      |> Enum.take(n)
+
+    rest =
+      data
+      |> Enum.drop(n)
+
+    IO.inspect({[], metacounts, metadata ++ more_meta, rest})
+    parse({[], metacounts, metadata ++ more_meta, rest})
+  end
+
+  def parse({[0 | counts], [n | metacounts], metadata, data}) do
+    more_meta =
+      data
+      |> Enum.take(n)
+
+    rest =
+      data
+      |> Enum.drop(n)
+
+    IO.inspect({counts, metacounts, metadata ++ more_meta, rest})
+    parse({counts, metacounts, metadata ++ more_meta, rest})
+  end
+
+  def parse({[c | counts], metacounts, metadata, [0, n | rest]}) do
+    more_meta =
+      rest
+      |> Enum.take(n)
+
+    data =
+      rest
+      |> Enum.drop(n)
+
+    next_counts = [c - 1 | counts]
+
+    IO.inspect({next_counts, metacounts, metadata ++ more_meta, data})
+    parse({next_counts, metacounts, metadata ++ more_meta, data})
+  end
+
+  def parse({[c | counts], metacounts, metadata, data}) do
+    [count, metacount] =
+      data
+      |> Enum.take(2)
+
+    next =
+      data
+      |> Enum.drop(2)
+
+    IO.inspect({[count, c - 1 | counts], [metacount | metacounts], metadata, next})
+
+    parse({[count, c - 1 | counts], [metacount | metacounts], metadata, next})
   end
 end
